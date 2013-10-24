e7aa18b87e54ca0cbd7f635974d84636bc0e48b6
manbropig <manbropig@gmail.com>
2013-10-23 20:45:47 -0700

    correction from previous commit, NOW table is set up properly with rating_sum, votes, rating, featured (boolean), and time

--- config/create.php
+++ config/create.php
@@ -14,19 +14,18 @@ $connector->drop_db($db_name);
 $connector->create_db($db_name);
 $connector->choose_db($db_name);
 $table_maker = "CREATE TABLE IF NOT EXISTS $table_name
-    (ID INTEGER(3),
+    (ID INTEGER(3) NOT NULL AUTO_INCREMENT,
     TITLE VARCHAR(30),
     AUTHOR VARCHAR(30),
     POEM VARCHAR(200),
     RATING_SUM DOUBLE DEFAULT 0,
     VOTES INTEGER(2) DEFAULT 0,
-    USER_RATING DOUBLE DEFAULT 0,
+    RATING DOUBLE DEFAULT 0,
     FEATURED BOOLEAN NOT NULL DEFAULT 0,
+    TIME INTEGER(12),
     PRIMARY KEY (ID))";
 $connector->create_table($table_maker);
 $connector->close_db();
 
-//what should the primary key be?
-//Should the same title be allowed for diff poems?
 ?>
 
